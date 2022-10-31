2c1d61275460fd125663aa386b03b90a317b232f
Ngo Anh Duy <112923029+Quizt242@users.noreply.github.com>
2022-10-31 23:33:39 +0700

    Update DBContext.java

--- My Project/src/java/dal/DBContext.java
+++ My Project/src/java/dal/DBContext.java
@@ -14,6 +14,7 @@ import java.util.logging.Logger;
 /**
  *
  * @author ngoan
+ * @param <T>
  */
 public abstract class DBContext<T> {
      protected Connection connection;
@@ -37,7 +38,7 @@ public abstract class DBContext<T> {
     public abstract T get(int id); // SELECT * FROM WHERE id = 
     public abstract ArrayList<T> list(); //override SELECT * FROM
 }
-
+   
 class Check {
     public static void main(String[] args) {
         DBContext db = new DBContext() {
